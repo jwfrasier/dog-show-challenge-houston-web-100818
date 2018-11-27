cccaa945597e4d840af5d7aaab4f8c2c4944106d
jwfrasier <frasier.joseph@gmail.com>
2018-11-27 10:26:49 -0600

    commited by getting the doge names on the screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
+let url = "http://localhost:3000/dogs";
+let tableBody = document.querySelector("#table-body");
 
-})
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    for (let index = 0; index < data.length; index++) {
+      let element = data[index];
+      let dogRow = document.createElement("tr");
+      let dogData = document.createElement("td");
+      let dogBreed = document.createElement("td");
+      let dogSex = document.createElement("td");
+      let dogButton = document.createElement("button");
+
+      dogData.innerHTML = element.name;
+      dogBreed.innerHTML = element.breed;
+      dogSex.innerHTML = element.sex;
+      dogButton.innerHTML = "Edit 🐶";
+
+      dogRow.appendChild(dogData);
+      dogRow.appendChild(dogBreed);
+      dogRow.appendChild(dogSex);
+      dogRow.appendChild(dogButton);
+      tableBody.appendChild(dogRow);
+    }
+  });
